cd781698430d09c96602247523308749f94c1776
0x4C6F6B69 <0x4C6F6B69@proton.me>
2025-09-09 19:57:09 +0200

    Defined SummaryResponse model type

---
+++ src/WebApi/Models/SummaryResponse.cs
@@ -0,0 +1,12 @@
+﻿using Peel.Domain;
+
+namespace Peel.Models;
+
+public class SummaryResponse
+{
+    public required List<OfferSummary> Summaries { get; init; }
+
+    public List<string>? Errors { get; init; }
+
+    public required decimal BtcUnitPrice { get; init; }
+}
