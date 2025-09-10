be6904dc4817302620d54bcff7182f2992af7ff5
0x4C6F6B69 <0x4C6F6B69@proton.me>
2025-09-10 20:38:09 +0200

    Implemented feature to get flat offer summaries

---
+++ src/Core/Domain/Models/OfferSummaryFlat.cs
@@ -0,0 +1,16 @@
+namespace Peel.Domain;
+
+public record class OfferSummaryFlat : OfferSummaryBase
+{
+    public required decimal QuoteAmountSat { get; init; }
+    public required decimal QuoteAmountBtc { get; init; }
+    public required decimal? QuotePriceFiatBase { get; init; }
+    public required decimal QuotePriceFiat { get; init; }
+
+    public decimal? QuoteMaxAmountSat { get; init; }
+    public decimal? QuoteMaxAmountBtc { get; init; }
+    public decimal? QuoteMaxPriceFiatBase { get; init; }
+    public decimal? QuoteMaxPriceFiat { get; init; }
+
+    public required string MeansOfPayment { get; init; }
+}
