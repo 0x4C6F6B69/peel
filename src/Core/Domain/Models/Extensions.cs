65279a09f7c17041e9cff11feed3909587897248
0x4C6F6B69 <0x4C6F6B69@proton.me>
2025-09-10 20:38:09 +0200

    Implemented feature to get flat offer summaries

---
+++ src/Core/Domain/Models/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Peel.Domain;
+
+public static class OfferSummaryExtension
+{
+    public static OfferSummaryFlat Flatten(this OfferSummary summary, JsonSerializerOptions jsonOptions) =>
+        new()
+        {
+            Id = summary.Id,
+            Type = summary.Type,
+            ReferenceId = summary.ReferenceId,
+            PublishingDate = summary.PublishingDate,
+            SpreadPc = summary.SpreadPc,
+            
+            QuoteAmountSat = summary.Quote.AmountSat,
+            QuoteAmountBtc = summary.Quote.AmountBtc,
+            QuotePriceFiatBase = summary.Quote.PriceFiatBase,
+            QuotePriceFiat = summary.Quote.PriceFiat,
+           
+            QuoteMaxAmountSat = summary.QuoteMax?.AmountSat,
+            QuoteMaxAmountBtc = summary.QuoteMax?.AmountBtc,
+            QuoteMaxPriceFiatBase = summary.QuoteMax?.PriceFiatBase,
+            QuoteMaxPriceFiat = summary.QuoteMax?.PriceFiat,
+            
+            MeansOfPayment = JsonSerializer.Serialize(summary.MeansOfPayment, jsonOptions)
+        };
+}
