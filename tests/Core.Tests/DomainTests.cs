81dd4104e8ddbb71fb75b7f679beb1bf123232b5
0x4C6F6B69 <0x4C6F6B69@proton.me>
2025-09-14 08:05:00 +0200

    Fixed a test implementing TolerantEqual method

--- tests/Core.Tests/DomainTests.cs
+++ tests/Core.Tests/DomainTests.cs
@@ -6,8 +6,9 @@ using SharpX.Extensions;
 using Xunit.Abstractions;
 using Converter = Peel.Infrastructure.Converter;
 
-public class DomainTests(ITestOutputHelper output)
+public class Domain_Tests(ITestOutputHelper output)
 {
+
     [Fact]
     public async Task Get_all_summary_types_searching_in_EUR() => _ = await GetOffersSummaryAndAssertAsync(
         new OfferSearchCriteriaDefault
@@ -30,31 +31,39 @@ public class DomainTests(ITestOutputHelper output)
     );
 
     [Fact]
-    public async Task Check_correctness_of_summaries_conversions_searching_in_EUR() => _ = await GetOffersSummaryAndAssertAsync(
-        new OfferSearchCriteriaDefault
-        {
-            Type = CriteriaType.Default,
-            OfferType = Peel.Domain.OfferTypeFilter.Buy,
-            Amount = new OfferAmount(CurrencyType.Fiat, 100m, 1000m),
-            MeansOfPayment = new() { ["EUR"] = ["sepa"] },
-        },
-        (summeries, btcUnitPrice) => Assert.All(summeries, summary =>
-        {
-            var quoteBtc = Converter.SatoshiToBitcoin((long)summary.Quote.AmountSat);
-            Assert.Equal(quoteBtc, summary.Quote.AmountBtc);
-            var quoteFiat = quoteBtc * btcUnitPrice;
-            Assert.Equal(quoteFiat, summary.Quote.PriceFiat);
-            var quoteSat = Converter.FiatToSatoshi(quoteFiat, btcUnitPrice);
-            Assert.Equal(quoteSat, (long)summary.Quote.AmountSat);
+    public async Task Check_correctness_of_summaries_conversions_searching_in_EUR()
+    {
+        const long SAT_TOLERANCE = 8;
 
-            var quoteMaxBtc = Converter.SatoshiToBitcoin((long)summary.QuoteMax!.AmountSat);
-            Assert.Equal(quoteMaxBtc, summary.QuoteMax.AmountBtc);
-            var quoteMaxFiat = quoteMaxBtc * btcUnitPrice;
-            Assert.Equal(quoteMaxFiat, summary.QuoteMax.PriceFiat);
-            var quoteMaxSat = Converter.FiatToSatoshi(quoteMaxFiat, btcUnitPrice);
-            Assert.Equal(quoteMaxSat, (long)summary.QuoteMax.AmountSat);
-        })
-);
+        _ = await GetOffersSummaryAndAssertAsync(
+            new OfferSearchCriteriaDefault
+            {
+                Type = CriteriaType.Default,
+                OfferType = Peel.Domain.OfferTypeFilter.Buy,
+                Amount = new OfferAmount(CurrencyType.Fiat, 100m, 1000m),
+                MeansOfPayment = new() { ["EUR"] = ["sepa"] },
+            },
+            (summeries, btcUnitPrice) =>
+            {
+                Assert.All(summeries, summary =>
+                {
+                    var quoteBtc = Converter.SatoshiToBitcoin((long)summary.Quote.AmountSat);
+                    Assert.Equal(quoteBtc, summary.Quote.AmountBtc);
+                    var quoteFiat = Math.Round(quoteBtc * btcUnitPrice, 2);
+                    Assert.Equal(quoteFiat, summary.Quote.PriceFiat);
+                    var quoteSat = Converter.FiatToSatoshi(quoteFiat, btcUnitPrice);
+                    AssertEx.TolerantEqual(quoteSat, (long)summary.Quote.AmountSat, SAT_TOLERANCE);
+
+                    var quoteMaxBtc = Converter.SatoshiToBitcoin((long)summary.QuoteMax!.AmountSat);
+                    Assert.Equal(quoteMaxBtc, summary.QuoteMax.AmountBtc);
+                    var quoteMaxFiat = Math.Round(quoteMaxBtc * btcUnitPrice, 2);
+                    Assert.Equal(quoteMaxFiat, summary.QuoteMax.PriceFiat);
+                    var quoteMaxSat = Converter.FiatToSatoshi(quoteMaxFiat, btcUnitPrice);
+                    AssertEx.TolerantEqual(quoteMaxSat, (long)summary.QuoteMax.AmountSat, SAT_TOLERANCE);
+                });
+            }
+        );
+    }
 
     private async Task<List<OfferSummary>> GetOffersSummaryAndAssertAsync(
         OfferSearchCriteria criteria, Action<IEnumerable<OfferSummary>, decimal>? assert = null, bool failOnEmpty = false)
