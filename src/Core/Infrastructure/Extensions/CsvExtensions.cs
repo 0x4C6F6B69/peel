f5be186892218f305216dac5dffacb41705d9bf3
0x4C6F6B69 <0x4C6F6B69@proton.me>
2025-09-14 06:14:44 +0200

    Reorganized code and source files

---
+++ src/Core/Infrastructure/Extensions/CsvExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace Peel.Infrastructure;
+
+public static class CsvExtensions
+{
+    /// <summary>
+    /// Converts a list of records to a CSV string.
+    /// </summary>
+    public static async Task<string> ToCsvTextAsync<T>(this IEnumerable<T> records)
+    {
+        await using var writer = new StringWriter();
+        using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+        csv.WriteRecords(records);
+        await writer.FlushAsync();
+
+        return writer.ToString();
+    }
+}
---
+++ src/Core/Infrastructure/Extensions/NumberExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Peel.Infrastructure;
+
+public static class NumberExtensions
+{
+    public static decimal Increase(this decimal value, decimal percent) => value * (1M + percent / 100M);
+}
