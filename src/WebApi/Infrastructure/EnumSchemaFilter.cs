205f3a618278962ef45f3a603dcff5b6b34d2629
0x4C6F6B69 <0x4C6F6B69@proton.me>
2025-09-21 19:20:13 +0200

    Updated Program.cs to improve swagger definition

---
+++ src/WebApi/Infrastructure/EnumSchemaFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+public class EnumSchemaFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        if (schema == null || context?.Type == null)
+            return;
+
+        var type = Nullable.GetUnderlyingType(context.Type) ?? context.Type;
+        if (!type.IsEnum)
+            return;
+
+        var names = Enum.GetNames(type);
+        var arr = new OpenApiArray();
+        foreach (var name in names) {
+            arr.Add(new OpenApiString(name));
+        }
+
+        // Add or replace the x-enumNames extension
+        schema.Extensions["x-enumNames"] = arr;
+    }
+}
